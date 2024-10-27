de6e6bae5cacb8ad4f2fa155f6fa772f142652f5
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-10-27 15:40:14 -0300

    Add files via upload

---
+++ Desafio 2.py
@@ -0,0 +1,6 @@
+'Crie  um script python que leia o dia, o mês e o ano de nascimento de uma pessoa, e mostre uma mensagem com a data formatada'
+
+dia=input('Qual o dia do seu aniversário?')
+mes=input('Qual o mês do seu aniversário?')
+ano=input('Qual o ano do seu aniversário?')
+print('Você nasceu', dia, 'de', mes, 'de', ano, 'correto?')
